e7f94945c89a6693c7f5ba94c61a17be00fbea8b
HildaReplika <hilda@replikatech.com>
2024-11-20 18:54:12 +0530

    software dev screen ui done

---
+++ src/components/softwareDevelopment/index.ts
@@ -0,0 +1,6 @@
+export { default as SoftwareDevBanner } from "./SoftwareDevBanner";
+export { default as SoftwareDevelopmentAbout } from "./SoftwareDevelopmentAbout";
+export { default as SoftwareDevServices } from "./SoftwareDevServices";
+export { default as SoftwareDevExperts } from "./SoftwareDevExperts";
+export { default as SoftwareDevStayAhead } from "./SoftwareDevStayAhead";
+export { default as SoftwareDevTechnologies } from "./SoftwareDevTechnologies";
