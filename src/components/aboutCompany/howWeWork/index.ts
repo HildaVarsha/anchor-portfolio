d330f001ef0db78d67f361eead6291f3f59fc0b5
HildaReplika <hilda@replikatech.com>
2024-12-27 18:28:31 +0530

    how we work ui done

---
+++ src/components/aboutCompany/howWeWork/index.ts
@@ -0,0 +1,4 @@
+export { default as HowWeWorkBanner } from "./HowWeWorkBanner";
+export { default as HowWeDoMethodologies } from "./HowWeDoMethodologies";
+export { default as HowWeDoCollaboration } from "./HowWeDoCollaboration";
+export { default as HowWeDoComaprison } from "./HowWeDoComaprison";
