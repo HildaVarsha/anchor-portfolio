0930e6879e45194f5797f6c4b35d9c65e9de36bd
HildaReplika <hilda@replikatech.com>
2024-12-29 11:34:10 +0530

    menus updated

---
+++ src/app/services/ai-dev/page.tsx
@@ -0,0 +1,13 @@
+import { AiDevAbout, AiDevBanner } from "@/components/services/aiDev";
+import React from "react";
+
+const AiDev = () => {
+  return (
+    <div>
+      <AiDevBanner />
+      <AiDevAbout />
+    </div>
+  );
+};
+
+export default AiDev;
---
+++ src/components/services/aiDev/index.ts
@@ -0,0 +1,3 @@
+export { default as AiDevBanner } from "./AiDevBanner";
+export { default as AiDevAbout } from "./AiDevAbout";
+export { default as AiDevAtAnchor } from "./AiDevAtAnchor";
