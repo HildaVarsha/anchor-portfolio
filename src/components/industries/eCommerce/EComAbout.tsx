a84965e18f17f9c5911a75beb12f9b77769b14c8
HildaReplika <hilda@replikatech.com>
2025-01-17 18:00:15 +0530

    e com ui done

---
+++ src/components/industries/eCommerce/EComAbout.tsx
@@ -0,0 +1,34 @@
+import { PageHeaderText } from "@/components/shared";
+import Image from "next/image";
+import React from "react";
+
+const EComAbout = () => {
+  return (
+    <div className="bg-white py-16 text-slate-800">
+      <div className="md:container mx-auto px-4">
+        <PageHeaderText label="Ecommerce development services for online and brick-and-mortar retailers" />
+        <p>
+          No matter what type of retail business you are, we have the right
+          expertise and experience to deliver a successful ecommerce solution
+          that will engage your specific audience — with a tailored
+          functionality, sleek design, and human-centered approach.
+        </p>
+        <div className="py-12">
+          <PageHeaderText label="What we build:" />
+          <Image
+            src={
+              "https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=2520&h=1500&dpr=2"
+            }
+            alt="E Commerce"
+            width={100}
+            height={100}
+            quality={100}
+            className="w-full h-[500px] object-cover rounded-md mt-12"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EComAbout;
---
+++ src/components/industries/eCommerce/EComAdopt.tsx
@@ -0,0 +1,71 @@
+import { ServicesCard } from "@/components/componentShared";
+import React from "react";
+const services = [
+  {
+    title: "Virtual fitting rooms",
+  },
+  {
+    title: "Product recommendations",
+  },
+  {
+    title: "AR product modelling",
+  },
+  {
+    title: "Voice shopping",
+  },
+  {
+    title: "Immersive product catalog",
+  },
+  {
+    title: "Virtual assistants and chatbots",
+  },
+  {
+    title: "AI-powered smart pricing",
+  },
+  {
+    title: "Predictive customer analytics",
+  },
+];
+
+const EComAdopt = () => {
+  return (
+    <div className="bg-white ">
+      <div
+        className="py-16 h-[400px] flex items-center"
+        style={{
+          backgroundImage: "url('/ecom-adopt.webp')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <p className="px-4 md:px-0 text-2xl md:text-5xl font-bold md:container mx-auto">
+          And adopting new tech
+          <span className="text-amber-400 pl-2">
+            {" "}
+            for ecommerce software solutions
+          </span>
+        </p>
+      </div>
+      <div className="py-12 bg-amber-400">
+        <p className="px-4 md:px-0 md:container mx-auto text-slate-800">
+          By empowering ecommerce development services with with battle-proven
+          technologies, including Adobe Commerce (ex Magento), we help you push
+          the boundaries with big ideas and bold changes to edge out the
+          toughest competition.
+        </p>
+      </div>
+      <div className="px-4 md:px-0 md:container mx-auto text-slate-800">
+        <div className="py-16 grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
+          {services.map((service, index) => (
+            <div className="text-2xl font-semibold" key={index}>
+              {service?.title}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EComAdopt;
