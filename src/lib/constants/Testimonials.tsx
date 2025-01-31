c1e88c1d6b27a836e7c40197d781499e0119aa61
HildaReplika <hilda@replikatech.com>
2025-01-31 11:42:32 +0530

    testimonial changes

--- src/lib/constants/Testimonials.tsx
+++ src/lib/constants/Testimonials.tsx
@@ -1,8 +1,8 @@
 export const TESTIMONIALS = [
   {
     avatar: "CN",
-    name: "Mounika",
-    company: "HR at Anchor Informatics Ltd",
+    name: "Rabo Bank",
+    company: "Client",
     text: "Working with Anchor Informatics Ltd was a transformative experience for our organization. Their team of professionals brought unmatched expertise to the table, turning our challenges into opportunities for growth. From the first meeting to the project handoff, they demonstrated a deep understanding of our requirements and provided innovative solutions tailored to our needs. The software development services they delivered were nothing short of exceptional, ensuring that our platform was robust, scalable, and future-proof. Their attention to detail and commitment to quality stood out throughout the process.",
   },
 ];
